dcfbcbc8157c0f78ddb1cc4044c2fbb82bd2191a
IvanGoFo <igonzalezforner@gmail.com>
2024-11-29 16:59:50 +0100

    feat: Barra de carga + pantalla menu

--- app/src/main/java/com/example/juegocolgado/Menu.kt
+++ app/src/main/java/com/example/juegocolgado/Menu.kt
@@ -1,25 +1,112 @@
 package com.example.juegocolgado
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun Menu(navController: NavController) {
     Box(
-        modifier = Modifier.fillMaxSize().background(Color.Red)) {
-        Text(
-            text = "Menu Screen",
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Brush.verticalGradient(listOf(Color.White, Color.LightGray))) // Fondo blanco a gris
+    ) {
+        Column(
             modifier = Modifier
-                .align(Alignment.Center)
-                .clickable
-                { navController.navigate(Routes.GameScreen.route) })
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box (
+                modifier = Modifier
+                    .size(300.dp)
+            ) {
+                Image(
+                    contentScale = ContentScale.FillBounds,
+                    painter = painterResource(
+                        id = R.drawable.hangnn
+                    ),
+                    contentDescription = "Game icon",
+                    modifier = Modifier.matchParentSize()
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .clickable { navController.navigate(Routes.GameScreen.route) }
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(0xFFDD6AE5),
+                                Color(0xFFAA00AA)
+
+                            )
+                        )
+                    )
+                    .padding(horizontal = 72.dp, vertical = 10.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Play",
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .clickable {  }
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(0xFFDD6AE5),
+                                Color(0xFFAA00AA)
+
+                            )
+                        )
+                    )
+                    .padding(horizontal = 72.dp, vertical = 10.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Help",
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/juegocolgado/Start.kt
+++ app/src/main/java/com/example/juegocolgado/Start.kt
@@ -2,29 +2,24 @@ package com.example.juegocolgado
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 
 @Composable
 fun Start(navController: NavController) {
@@ -40,9 +35,10 @@ fun Start(navController: NavController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Box (
+            // Game icon image
+            Box(
                 modifier = Modifier
-                    .size(300.dp)
+                    .size(200.dp)
             ) {
                 Image(
                     contentScale = ContentScale.FillBounds,
@@ -53,45 +49,51 @@ fun Start(navController: NavController) {
                     modifier = Modifier.matchParentSize()
                 )
             }
-            // Título principal
-            Text(
-                text = "Welcome!",
-                style = MaterialTheme.typography.displayMedium,
-                color = Color.Black,
-                fontWeight = FontWeight.Bold,
 
-                modifier = Modifier
-                    .padding(8.dp) // Espaciado para que la sombra no quede muy pegada
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-
-
-            // Botón con diseño neutral
+            // Fake progress bar
             Box(
-                modifier = Modifier
-                    .clickable { navController.navigate(Routes.MenuScreen.route) }
-                    .clip(RoundedCornerShape(12.dp))
-                    .background(
-                        brush = Brush.horizontalGradient(
-                            colors = listOf(
-                                Color(0xFFDD6AE5),
-                                Color(0xFFAA00AA)
-
-                            )
-                        )
-                    )
-                    .padding(horizontal = 32.dp, vertical = 12.dp),
-                contentAlignment = Alignment.Center
+                modifier = Modifier.fillMaxWidth().padding(top = 16.dp)
             ) {
-                Text(
-                    text = "Start",
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                )
+                FakeProgressBar(navController)
             }
-
         }
+    }
+}
+
+@Composable
+fun FakeProgressBar(navController: NavController) {
+    var progressStatus by remember { mutableFloatStateOf(0f) }
+
+    // Update the progress status in a loop with a delay
+    LaunchedEffect(Unit) {
+        while (progressStatus < 0.8f) {
+            delay(50)
+            progressStatus += 0.03f
+        }
+        delay(600)
+        while (progressStatus < 1f) {
+            delay(50)
+            progressStatus += 0.01f
+        }
+        navController.navigate(Routes.MenuScreen.route)
+    }
+
+    // Progress bar UI
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        LinearProgressIndicator(
+            progress = progressStatus,
+            color = Color.Magenta,
+            trackColor = Color.LightGray,
+            strokeCap = StrokeCap.Butt,
+            modifier = Modifier
+                .height(8.dp)
+                .fillMaxWidth()
+
+        )
     }
 }
